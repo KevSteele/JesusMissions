0a5ea2846c1ab6811a1d14287749afee484a0b67
Kevin Steele <kevin.steele@ramseysolutions.com>
2023-06-09 09:31:22 -0500

    added react native maps

--- Pages/FindYourMission.js
+++ Pages/FindYourMission.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { styles } from '../App';
+import MapView from 'react-native-maps';
 
 const FindYourMission = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text>Find Your Mission</Text>
+      <MapView
+        style={{ flex: 1, backgroundColor: "blue" }}
+        initialRegion={{
+          latitude: 28.0000,
+          longitude: 82.0000,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 };
