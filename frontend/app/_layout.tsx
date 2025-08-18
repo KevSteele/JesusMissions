bae5530b6b33ddb2b4ee04f4c01674a123be953c
KevSteele <kevins.steele@gmail.com>
2025-08-18 17:37:31 -0500

    upgrade to expo sdk 53

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,11 +1,12 @@
+
+import 'react-native-gesture-handler';
+import 'react-native-reanimated';
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; // Import React Query components
 import { useColorScheme } from '@/components/useColorScheme';
 
@@ -57,11 +58,9 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+    </Stack>
   );
 }
